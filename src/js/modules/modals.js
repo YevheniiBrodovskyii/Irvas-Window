7aee9682b55d3e13cea884c3696db56f2829f732
YevheniiBrodovskyii <zhenyabroddd@gmail.com>
2022-03-20 17:04:21 +0100

    Finish project and compile Dist version

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,7 +4,8 @@ const modals = () => {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            windows = document.querySelectorAll('[data-modal]');
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll();
         
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
@@ -18,6 +19,7 @@ const modals = () => {
 
                 modal.style.display = "block";
                 document.body.style.overflow = "hidden";
+                document.body.style.marginRight = `${scroll}px`;
                 // document.body.classList.add('modal-open');
             });
         });
@@ -25,6 +27,7 @@ const modals = () => {
         close.addEventListener('click', () => {
             modal.style.display = "none";
             document.body.style.overflow = "";
+            document.body.style.marginRight = `0px`;
             // document.body.classList.remove('modal-open');
 
             windows.forEach(item => {
@@ -37,6 +40,7 @@ const modals = () => {
             if (e.target === modal && closeClickOverlay) {
                 modal.style.display = "none";
                 document.body.style.overflow = "";
+                document.body.style.marginRight = `0px`;
                 // document.body.classList.remove('modal-open');
 
                 windows.forEach(item => {
@@ -51,6 +55,21 @@ const modals = () => {
             document.querySelector(selector).style.display= "block";
             document.body.style.overflow = "hidden";
         }, time);
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
     }
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
